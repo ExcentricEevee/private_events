0470a4685cb89c39bd2eb188021d5c3664d0508a
Curtis <excentriceevee@hotmail.com>
2023-06-21 16:03:24 -0400

    Connect events to creators and show a user's events on their show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_011909) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_21_065226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_011909) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
     t.index ["title"], name: "index_events_on_title", unique: true
   end
 
