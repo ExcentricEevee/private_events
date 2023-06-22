f5f0c0310e93a85acf6bc3abf316ff7c3d340143
Curtis <excentriceevee@hotmail.com>
2023-06-22 00:55:56 -0400

    Create event#new form, establish controller filter for members only

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,12 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+
     def index
         @events = Event.all
     end
 
     def show
-        @event = Event.find(param[:id])
+        @event = Event.find(params[:id])
     end
 
     def new
