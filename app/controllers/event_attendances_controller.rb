e764f7147c025a0fc77b7d5b664a3aeede30581c
Curtis <excentriceevee@hotmail.com>
2023-07-01 19:09:21 -0400

    Have EventAttancesController check for duplicate attendance attempts

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -5,13 +5,20 @@ class EventAttendancesController < ApplicationController
         @event = Event.find(params[:event_id])
         @attendee = EventAttendance.new(attendee_id: current_user.id, event_id: @event.id)
 
+        # Don't allow the same user to join multiple times
+        # Is there a better way to check for existance?
+        if EventAttendance.exists?(attendee_id: current_user.id, event_id: @event.id)
+            flash.now[:alert] = "You have already joined this event!"
+            render "events/show", status: :unprocessable_entity
+        else
             if @attendee.save
                 flash[:notice] = "Event joined successfully!"
                 redirect_to @event
             else
                 flash.now[:alert] = "There was a problem joining the event"
-                render @event, status: :unprocessable_entity
+                render "events/show", status: :unprocessable_entity
             end
+        end
     end
 
     def destroy
