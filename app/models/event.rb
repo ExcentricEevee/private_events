00d1597655e23f577a494fa00c3777fbb30d1c13
Curtis <excentriceevee@hotmail.com>
2023-07-02 01:26:39 -0400

    Have associations set to be destroyed along with their origins, ie destroyed Users lose their creations and attendances

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
-    has_many :event_attendance, foreign_key: :event_id
+    has_many :event_attendance, foreign_key: :event_id, dependent: :destroy
     has_many :attendees, through: :event_attendance, source: :attendee
 
     validates :title, :description, :date, :location, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :created_events, class_name: "Event", foreign_key: :creator_id
-  has_many :event_attendance, foreign_key: :attendee_id
+  has_many :created_events, class_name: "Event", foreign_key: :creator_id, dependent: :destroy
+  # You should only have to make a dependence here on the through table for destruction purposes
+  has_many :event_attendance, foreign_key: :attendee_id, dependent: :destroy
   has_many :attended_events, through: :event_attendance, source: :event
   
   validates :username, presence: true, length: { in: 2..24 }
