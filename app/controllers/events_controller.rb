94bf45539f5769a44ff32af5354c7e555d203a5b
Curtis <excentriceevee@hotmail.com>
2023-07-04 15:50:19 -0400

    Allow users to edit their own events
    Controller filters have been added to give a level of authorization to
    this functionality, otherwise users would be able to edit each other's
    posts so long as they knew how to manipulate the URL.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,8 @@
 class EventsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
+    # Users should only be able to edit/destroy their own events
+    before_action :set_event, only: [:edit, :update, :destroy]
+    before_action :redirect_unless_owner, only: [:edit, :update, :destroy]
 
     def index
         @events = Event.all
@@ -17,15 +20,46 @@ class EventsController < ApplicationController
         @event = current_user.created_events.build(event_params)
 
         if @event.save
-            redirect_to root_path
+            redirect_to @event
         else
             render :new, status: :unprocessable_entity
         end
+    end
+
+    def edit
+        @event = Event.find(params[:id])
+        # is there a better way to check for authorization?
+
+    end
+
+    def update
+        @event = Event.find(params[:id])
+
+        if @event.update(event_params)
+            redirect_to @event
+        else
+            flash.now[:alert] = "There was an error updating your event"
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+
     end
 
     private
 
     def event_params
         params.require(:event).permit(:title, :description, :date, :location)
+    end
+
+    def set_event
+        @event = Event.find(params[:id])
+    end
+
+    def redirect_unless_owner
+        return if @event.creator.id == current_user.id
+
+        redirect_to root_path, alert: "You're not authorized to do that"
     end
 end
